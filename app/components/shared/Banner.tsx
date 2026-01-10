60a76f9a4bd5d0cf0d9a72630a18106a159116f8
shiuly-28 <shulybd1245@gmail.com>
2026-01-10 18:00:15 +0600

    banner part designe done

---
+++ app/components/shared/Banner.tsx
@@ -0,0 +1,25 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+import Image from 'next/image'
+import bannerImg from '@/public/banner.jpeg'
+const Banner = () => {
+  return (
+    <div className='bg-slate-100'>
+    <div className='px-4 py-8 lg:px-8 grid grid-cols-1 md:grid-cols-2 items-center gap-8'>
+       <div>
+        <Image src={bannerImg} alt='banner'   width={500}
+      height={500}/>
+       </div>
+     <div className='flex flex-col space-y-4'>
+      <h4 className='text-sm font-medium text-gray-500'>Technology</h4>
+     <h2 className='text-2xl font-bold'>OpenAi is going Fast and Burning Through Piles of Money</h2>
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae vero voluptatum dolore iste quisquam cupiditate, ab nisi doloribus, <br /> quibusdam doloremque velit alias cum ullam sequi asperiores, quos rerum eligendi delectus sed. Saepe, sunt? Quidem quia hic laboriosam reprehenderit aspernatur. Officiis.</p>
+     <Button variant="default">Read More</Button>
+     </div>
+     
+    </div>
+    </div>
+  )
+}
+
+export default Banner
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import Banner from "./components/shared/Banner";
 
 
 export default function Home() {
   return (
-   <div className="text-3xl">
-     <h1 className="text-red-400">Hello, Next Js Developers</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, repellendus libero! Odit, illo nobis in illum eaque eligendi! Cum id quis praesentium, sint ea pariatur? Nemo quibusdam similique incidunt sapiente?</p>
+   <div className="py-12">
+    <Banner/>
    </div>
   );
 }
